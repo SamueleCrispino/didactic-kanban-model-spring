b5e062a989f80b9b730b28a4b2967353e4098796
SamueleCrispino <samuele.crispino94@gmail.com>
2021-07-16 11:13:27 +0200

    modified

--- KanbanBoard/src/main/java/com/unimib/KanbanBoard/KanbanBoardApplication.java
+++ KanbanBoard/src/main/java/com/unimib/KanbanBoard/KanbanBoardApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//Entry Point
+//Entry Point !
 @SpringBootApplication
 @EnableJpaAuditing
 public class KanbanBoardApplication {
